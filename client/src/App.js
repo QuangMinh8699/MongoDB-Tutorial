27720f444ad8a8f247534211e66d0139ee558d6e
QuangMinh <93485271+QuangMinh8699@users.noreply.github.com>
2022-05-02 22:21:12 +0700

    update

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Landing from "./components/layout/Landing";
+import Login from "./components/auth/Login";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing replace to="/login" />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
